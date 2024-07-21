de922f6563d6e40d7aa2278a0bedb2104629fa76
sitjan <sitaulajan@gmail.com>
2024-07-21 20:05:58 +0545

    struct concepts

--- sweb.go
+++ sweb.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "New try")
-}
-func contact_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "try contacting my mail")
+type car struct {
+	gas_pedal      uint16
+	brake_pedal    uint16
+	steering_wheel int16
+	top_speed_kmh  float64
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/contact", contact_handler)
-	http.ListenAndServe(":8000", nil)
+	a_car := car{gas_pedal: 2215, brake_pedal: 0, steering_wheel: 1022, top_speed_kmh: 29.21}
+
+	fmt.Println(a_car.gas_pedal)
 }
